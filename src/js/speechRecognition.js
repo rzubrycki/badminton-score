76c895ab38aae023ff8e48a52da6dc0be3e7b9c9
Robert Zubrycki <robert.zubrycki@intive.com>
2020-01-23 15:02:34 +0100

    Fixes

--- src/js/speechRecognition.js
+++ src/js/speechRecognition.js
@@ -1,10 +1,23 @@
-import { SuccessSound, ErrorSound, EndGameSound } from './sounds';
+import ConfettiGenerator from 'confetti-js';
+
+import { SuccessSound, ErrorSound, EndGameSound } from '../const/sounds';
 import { ScoreValues } from '../const/scoreValues';
 
-const whiteScore = [];
-const blackScore = [];
-const whitePlayerScoreBoard = document.querySelector('.player1');
-const blackPlayerScoreBoard = document.querySelector('.player2');
+const whitePlayer = {
+  points: [],
+  sets: []
+};
+const blackPlayer = {
+  points: [],
+  sets: []
+};
+const whitePlayerSets = document.querySelector('.player1-sets');
+const blackPlayerSets = document.querySelector('.player2-sets');
+const whitePlayerPoints = document.querySelector('.player1-points');
+const blackPlayerPoints = document.querySelector('.player2-points');
+
+const confettiSettings = { target: 'fireworks', max: 50, clock: 40 };
+const confetti = new ConfettiGenerator(confettiSettings);
 
 export function speechRecognition() {
   window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
@@ -12,8 +25,7 @@ export function speechRecognition() {
   recognition.interimResults = true;
   recognition.lang = 'pl-PL';
 
-  whitePlayerScoreBoard.textContent = whiteScore.length;
-  blackPlayerScoreBoard.textContent = blackScore.length;
+  updatePointsElements();
 
   recognition.addEventListener('result', e => {
     const transcript = Array.from(e.results)
@@ -23,26 +35,64 @@ export function speechRecognition() {
 
     if (e.results[0].isFinal) {
       if (transcript.includes(ScoreValues.whiteScore)) {
-        whiteScore.push(ScoreValues.addScore);
-        whitePlayerScoreBoard.textContent = whiteScore.length;
+        whitePlayer.points.push(ScoreValues.addScore);
+        whitePlayerPoints.textContent = whitePlayer.points.length;
         SuccessSound.play();
       } else if (transcript.includes(ScoreValues.blackScore)) {
-        blackScore.push(ScoreValues.addScore);
-        blackPlayerScoreBoard.textContent = blackScore.length;
+        blackPlayer.points.push(ScoreValues.addScore);
+        blackPlayerPoints.textContent = blackPlayer.points.length;
         SuccessSound.play();
       }
+      handleGameFlow();
+      handleEndGame();
     }
-
-    // ErrorSound.play();
   });
 
   recognition.addEventListener('end', recognition.start);
   recognition.start();
 }
 
-export const resetScoreBoard = () => {
-  whiteScore.length = 0;
-  blackScore.length = 0;
-  whitePlayerScoreBoard.textContent = whiteScore.length;
-  blackPlayerScoreBoard.textContent = blackScore.length;
-};
+export function resetScoreBoard() {
+  whitePlayer.points = [];
+  blackPlayer.points = [];
+  whitePlayer.sets = [];
+  blackPlayer.sets = [];
+
+  updatePointsElements();
+  updateSetsElements();
+  confetti.clear();
+  location.reload();
+}
+
+// helpers functions
+function handleGameFlow() {
+  if (whitePlayer.points.length === 21) {
+    whitePlayer.sets.push(ScoreValues.addScore);
+    whitePlayer.points = [];
+    blackPlayer.points = [];
+    updatePointsElements();
+  } else if (blackPlayer.points.length === 21) {
+    blackPlayer.sets.push(ScoreValues.addScore);
+    whitePlayer.points = [];
+    blackPlayer.points = [];
+    updatePointsElements();
+  }
+  updateSetsElements();
+}
+
+function handleEndGame() {
+  if (whitePlayer.sets.length === 2 || blackPlayer.sets.length === 2) {
+    EndGameSound.play();
+    confetti.render();
+  }
+}
+
+function updatePointsElements() {
+  whitePlayerPoints.textContent = whitePlayer.points.length;
+  blackPlayerPoints.textContent = blackPlayer.points.length;
+}
+
+function updateSetsElements() {
+  whitePlayerSets.textContent = whitePlayer.sets.length;
+  blackPlayerSets.textContent = blackPlayer.sets.length;
+}
